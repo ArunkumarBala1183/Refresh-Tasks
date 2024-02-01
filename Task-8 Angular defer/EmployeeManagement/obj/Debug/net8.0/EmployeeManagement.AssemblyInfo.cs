70490ab8cb9ef7f08196d2eb1c50349349414f50
Arunkumar Bala <arunkumar.bala@aspiresys.com>
2024-02-01 09:16:13 +0530

    Updated Nunit

--- Task-8 Angular defer/EmployeeManagement/obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
+++ Task-8 Angular defer/EmployeeManagement/obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d75ab2e8fbcf59289677c89cf6fb8b7a9649374")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+301af50c8aa26c1cfeeb25a0e3ba139c48f35039")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
